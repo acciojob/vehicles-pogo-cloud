1b777227730a490f3f9c31e915313cc4c0a5554c
Deeptesh Biswas <deepteshwas3@gmail.com>
2023-02-23 02:12:25 +0530

    changesdone

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -7,8 +7,9 @@ public class F1 extends Car {
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        newSpeed=getCurrentSpeed()+rate;
+         //set the value of new speed by using currentSpeed and rate
+        int newSpeed=getCurrentSpeed()+rate;
+       // setCurrentSpeed(newSpeed);
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,29 +19,26 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-
+        //for all other cases, change the gear accordingly
+        if(newSpeed>0&&newSpeed<=50){
+            changeGear(1);
+        }else if(newSpeed>=51&&newSpeed<=100){
+            changeGear(2);
+        }else if(newSpeed>=101&&newSpeed<=150){
+            changeGear(3);
+        }else if(newSpeed>=151&&newSpeed<=200){
+            changeGear(4);
+        }else if(newSpeed>=201&&newSpeed<=250){
+            changeGear(5);
+        }else if(newSpeed>250){
+            changeGear(6);
+        }
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
             stop();
             changeGear(1);
         }
-        //for all other cases, change the gear accordingly
-        int newGear=1;
-        if(newSpeed>0&&newSpeed<=50){
-            newGear=1;
-        }else if(newSpeed>=51&&newSpeed<=100){
-            newGear=2;
-        }else if(newSpeed>=101&&newSpeed<=150){
-               newGear=3;
-        }else if(newSpeed>=151&&newSpeed<=200){
-              newGear=4;
-        }else if(newSpeed>=201&&newSpeed<=250){
-             newGear=5;
-        }else if(newSpeed>250){
-            newGear=6;
-        }
-        if(newSpeed > 0) {
-            changeGear(newGear);
+        else if (newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
     }
